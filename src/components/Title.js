19bd2c39d83d08a59e02b10cf0f713c6b0c15f37
CallMeHarsh <harshfrendzon@gmail.com>
2021-04-13 21:27:29 +0530

    after long time

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ProfileImage2 from "../assets/ProfileImage2.jpg";
+import ProfileImage1 from "../assets/ProfileImage1.png";
 
 function Title() {
     return(
         <div className="container">
         <div className="row text-center align-items-center my-5 py-5">
             <div className="col-12 col-md-6">
-                <img className="image-fluid rounded-circle w-75 h-75" src={ProfileImage2} />
+                <img className="image-fluid rounded-circle w-75 h-75" src={ProfileImage1} />
             </div>
             <div className="col-12 col-md-6">
                 <div className="font-weight-light" style={{fontSize: "50px"}}>
