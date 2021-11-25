66d481aff406df1a0f7c08f2699d8f8cc6b42d63
CallMeHarsh <harshfrendzon@gmail.com>
2021-11-25 12:20:50 +0530

    links

--- src/components/ProjectPage.js
+++ src/components/ProjectPage.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Consumer } from "../context";
 import ReactMarkdown from "react-markdown";
+import { Link } from "react-router-dom";
+import Slide from 'react-reveal/Slide';
+
 
 function ProjectPage(props) {
   return (
@@ -12,6 +15,11 @@ function ProjectPage(props) {
         const { imageUrl, title, body, link } = project;
         return (
           <div className="container py-5 my-5 markdown ">
+            <Slide left>
+                <Link to="https://callmeharsh.github.io/reactStatic/">
+                        <button className="btn btn-outline-warning bt-lg btn-block">
+                        <i class="fa fa-arrow-circle-left fa-lg" aria-hidden="true"></i>  Go to Portfolio</button>
+                    </Link></Slide>
             <h1 className="font-weight-light text-center my-5 text-warning">{title}</h1>
             <div className="justify-content-center">
               <img src={imageUrl} alt={title} className="w-25 projectpic" />
